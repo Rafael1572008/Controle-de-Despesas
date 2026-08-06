375a028081d568c5891f5297be22d025c460a1c3
Renan Trajano <nantrajano@gmail.com>
2026-08-06 20:06:12 -0300

    Base da edição

--- src/core/urls.py
+++ src/core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('despesas/', lista_despesas, name='lista_despesas'),
     path('despesas/novo/', criar_despesa, name='criar_despesa'),
     path('despesas/<int:id>/', detalhe_despesa, name='detalhe_depesa'),
+    path('despesas/editar/<int:id>/', editar_despesa, name='editar_despesa')
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -30,6 +30,23 @@ def criar_despesa(request):
     return render(request, 'core/form_despesa.html', {'form': form})
 # Create your views here.
 
+@login_required
 def detalhe_despesa(request, id):
     despesa = get_object_or_404(Despesa, id=id)
     return render(request, 'core/detalhe_despesa.html', {'despesa': despesa})
+
+@login_required
+def editar_despesa(request, id):
+    despesa = get_object_or_404(Despesa, id=id)
+
+    if request.method == 'POST':
+        form = DespesaForm(request.POST, instance=despesa)
+
+        if form.is_valid():
+            form.save()
+            return redirect('lista_despesas')
+
+    else:
+        form = DespesaForm(instance=despesa)
+
+    return render(request, 'core/form_despesa.html', {'form': form})
