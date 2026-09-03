e073b5a8004e0b7360b2ba8bdd822f4054270fac
ThiagoIFDS23 <129286938+ThiagoIFDS23@users.noreply.github.com>
2026-09-03 17:47:37 -0300

    fix: rota "/sobre" removida

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,7 +3,6 @@ from .views import * # Puxar tudo de views
 
 urlpatterns = [
     path('', inicio, name='inicio'),
-    path('sobre/', sobre, name='sobre'),
 
     # Despesas
     path('despesas/', lista_despesas, name='lista_despesas'),
